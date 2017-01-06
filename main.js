9fbd78b7c134d0d4a72804b5b0229e4c4eacc175
kokihoon <rhrlgns777@naver.com>
2017-01-06 21:30:25 +0900

    prototype

--- main.js
+++ main.js
@@ -3,42 +3,66 @@ const path = require('path')
 const url = require('url')
 const fs = require('fs')
 
-//const WindowStateManager = require('electron-window-state-manager');
-
 let win
-let i = 0;
-app.on('ready', createWindow);
+app.on('ready', function(){
+  fs.readdir("./data", (err, files) => {
+    if(files.length>0){
+      // console.log("exist files");
 
-function createWindow (event) {
-  win = new BrowserWindow({width: 400, height: 300, frame : false})
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+      for(file in files){
+        createWindow(files[file]);
 
-  // win.setTitle(i+"");
-  // if(!fs.existsSync('./data/' +i + '.txt')) {
-  //   var data = "";
-  //   fs.writeFileSync('./data/' +i + '.txt', data, 'utf8');
-  // }
-  //
-  // i++;
-  // console.log(win.getTitle());
+      }
+    }else{
+      // console.log("no files");
+      createWindow();
+    }
+  })
+});
+
+
+function createWindow (fileName) {
+  if(fileName != null){
+    win = new BrowserWindow({width: 400, height: 300, frame : false});
+    win.setTitle(fileName);
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  }else{
+    // console.log("no file name");
+    win = new BrowserWindow({width: 400, height: 300, frame : false})
+    win.setTitle("");
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  }
 
   win.webContents.openDevTools()
 }
 
-
-ipcMain.on('save', function save(event, arg) {
-  console.log("save");
-
-  event.sender.send('close', 'close')
-
-  // if(fs.existsSync('./data/' +i + '.txt')) {
-  //   fs.writeFileSync('./data/' +i + '.txt', arg, 'utf8');
-  //   event.sender.send('close', 'close')
-  // }
+ipcMain.on('newSticky', function(event, content){
+  createWindow();
 });
 
-ipcMain.on('sync', createWindow);
+//저장하기
+ipcMain.on('save', function save(event, content) {
+  var random;
+  if(content != ""){
+    random = Math.random() * 10000;
+    fs.writeFileSync("./data/"+random+".txt", content);
+  }
+  event.sender.send('saveComplete', random+".txt");
+});
+
+//저장하고 종료
+ipcMain.on('saveClose', function save(event, content) {
+  if(content != ""){
+    let random = Math.random() * 10000;
+    fs.writeFileSync("./data/"+random+".txt", content);
+  }
+  event.sender.send('saveCloseComplete', 'saveCloseComplete');
+});
